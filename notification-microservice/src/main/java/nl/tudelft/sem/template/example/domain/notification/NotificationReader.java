bc213d6d60c928cc0e69c06586ab1e734a71502c
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-12-06 12:39:42 +0100

    Create a template for notification microservice.

---
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/example/domain/notification/NotificationReader.java
@@ -0,0 +1,15 @@
+package nl.tudelft.sem.template.example.domain.notification;
+
+public class NotificationReader {
+    private final NotificationStatus status;
+
+    public NotificationReader(NotificationStatus status) {
+        this.status = status;
+    }
+
+    public String read() {
+        //TODO
+        //implement messages based on the status
+        return "";
+    }
+}
---
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/example/domain/notification/NotificationStatus.java
@@ -0,0 +1,8 @@
+package nl.tudelft.sem.template.example.domain.notification;
+
+public enum NotificationStatus {
+    ACCEPTED,
+    REJECTED,
+    WITHDRAWN,
+    KICKED
+}
