70fee68bb5c96aadd26d3a35b8e05a8f7ab87908
Khalit Gulamov <K.Gulamov@student.tudelft.nl>
2022-12-07 18:26:19 +0100

    Pipeline fix

--- notification-microservice/src/main/java/rowing/notification/domain/notification/NotificationReader.java
+++ notification-microservice/src/main/java/rowing/notification/domain/notification/NotificationReader.java
@@ -1,12 +1,17 @@
 package rowing.notification.domain.notification;
 
 public class NotificationReader {
-    private transient final NotificationStatus status;
+    private transient NotificationStatus status;
 
     public NotificationReader(NotificationStatus status) {
         this.status = status;
     }
 
+    /**
+     * Converts notification to the string based on the status.
+     *
+     * @return The notification as a string message
+     */
     public String read() {
         //TODO
         //implement messages based on the status
