e5747f7dadb7131d7b9c15528295b0dec1debe30
Alexandru Ojica <aojica65@gmail.com>
2022-12-08 17:13:08 +0100

    add support for listeners

---
+++ kafka-template/src/main/java/nl/tudelft/sem/template/example/config/KafkaConsumerConfig.java
@@ -0,0 +1,58 @@
+package nl.tudelft.sem.template.example.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.sasl.jaas-config}")
+    private String kafkaJaasConfig;
+
+    @Value("${spring.kafka.properties.sasl-mechanism}")
+    private String kafkaSaslMechanism;
+
+    @Value("${spring.kafka.properties.security-protocol}")
+    private String kafkaSecurityProtocol;
+
+    @Value("${useAuthentication}")
+    private boolean useAuthentication;
+
+    public Map<String, Object> consumerConfig(){
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        if(useAuthentication) {
+            props.put("sasl.mechanism", kafkaSaslMechanism);
+            props.put("sasl.jaas.config", kafkaJaasConfig);
+            props.put("security.protocol", kafkaSecurityProtocol);
+        }
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> factory(ConsumerFactory<String, String> consumerFactory){
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+}
--- kafka-template/src/main/java/nl/tudelft/sem/template/example/config/KafkaProducerConfig.java
+++ kafka-template/src/main/java/nl/tudelft/sem/template/example/config/KafkaProducerConfig.java
@@ -5,9 +5,12 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,11 +21,28 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.properties.sasl.jaas-config}")
+    private String kafkaJaasConfig;
+
+    @Value("${spring.kafka.properties.sasl-mechanism}")
+    private String kafkaSaslMechanism;
+
+    @Value("${spring.kafka.properties.security-protocol}")
+    private String kafkaSecurityProtocol;
+
+    @Value("${useAuthentication}")
+    private boolean useAuthentication;
+
     public Map<String, Object> producerConfig(){
         HashMap<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        if(useAuthentication) {
+            props.put("sasl.mechanism", kafkaSaslMechanism);
+            props.put("sasl.jaas.config", kafkaJaasConfig);
+            props.put("security.protocol", kafkaSecurityProtocol);
+        }
         return props;
     }
 
