93b6b1b0ebc4693a40993276b2165b211d55cee2
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2023-01-17 12:22:58 +0100

    Deleted hello world

--- activity-microservice/src/main/java/rowing/activity/services/ActivityService.java
+++ activity-microservice/src/main/java/rowing/activity/services/ActivityService.java
@@ -58,15 +58,6 @@ public class ActivityService {
     }
 
     /**
-     * Gets example by id.
-     *
-     * @return the example found in the database with the given id
-     */
-    public String hellWorld() {
-        return "Hello " + authManager.getUsername();
-    }
-
-    /**
      * Method to create a new activity and add it to the repository.
      *
      * @param dto that will contain basic activity information
